66b7ddc37869c5a66f8ef719ca586ae21ffcb768
hetbabariya <hetbabriyabali09@gmail.com>
2025-01-20 19:09:05 +0530

    backend-setup

---
+++ backend/app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+from config import Config
+
+# Initialize extensions
+db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # Initialize extensions
+    db.init_app(app)
+    jwt.init_app(app)
+    migrate.init_app(app, db)
+
+
+    # Register blueprints here
+    from app.api import api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
+
+    return app
